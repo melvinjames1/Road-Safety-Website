96123828fc7b6d873b72fc9bd728fd7e6b090a0c
melvinjames1 <melvin.mj576@gmail.com>
2024-02-20 20:46:56 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { HomeComponent } from './home/home.component';
+import { BikesComponent } from './bikes/bikes.component';
+import { CarsComponent } from './cars/cars.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {'path': 'navbar', component: NavbarComponent},
+     {'path': '', component: HomeComponent},
+     {'path': 'bikes', component: BikesComponent},
+     {'path': 'cars', component: CarsComponent},
+     {'path':'about',component: AboutComponent}
+];
---
+++ src/app/bikes/bikes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bikes',
+  standalone: true,
+  imports: [],
+  templateUrl: './bikes.component.html',
+  styleUrl: './bikes.component.css'
+})
+export class BikesComponent {
+
+}
